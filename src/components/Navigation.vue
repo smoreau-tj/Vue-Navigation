d5beed52772fc1c418c409648643c81d8db3f137
davidhickey <davidhickey423@gmail.com>
2020-05-04 17:10:57 -0400

    adds HTML scaffolding for navigation

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,7 +1,21 @@
 <template>
-<h1>This is my navigation!!!</h1>
-
-
+  <header class="header-main" data-header-main>
+    <div class="header-main__inner container">
+      <div class="header-mobile-left"></div>
+      <div class="header-desktop-left">
+        <ul class="header-main-menu"></ul>
+         <a href="/" title="Tommy John Home" class="header-logo" aria-label="Tommy John Logo" tabindex="0">
+          </a>
+      </div>
+      <ul class="header-desktop-right">
+        <li class="header-list-item"></li>
+        <li class="header-list-item"></li>
+        <li class="header-list-item"></li>
+        <li class="header-list-item"></li>
+      </ul>
+      <div class="header-mobile-right"></div>
+    </div>
+  </header>
 </template>
 
 <script>
@@ -9,3 +23,13 @@ export default {
   name: 'Navigation',
 }
 </script>
+
+
+<style lang="scss">
+
+.header-main {
+  margin-top: 32px;
+}
+
+
+</style>
