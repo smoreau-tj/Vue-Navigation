146da0d465b264ad639759da3e8ea20066aabeec
davidhickey <davidhickey423@gmail.com>
2020-05-04 17:48:46 -0400

    adds spacing styling to header desktop elements

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -2,17 +2,26 @@
   <header class="header-main" data-header-main>
     <div class="header-main__inner container">
       <div class="header-mobile-left"></div>
-      <div class="header-desktop-left">
-        <ul class="header-main-menu"></ul>
-         <a href="/" title="Tommy John Home" class="header-logo" aria-label="Tommy John Logo" tabindex="0">
-          </a>
+      <div class="header-desktop-item left">
+        <ul class="header-desktop__list">
+          <li class="list-item"><a href="">Example 1</a></li>
+          <li class="list-item"><a href="">Example 2</a></li>
+          <li class="list-item"><a href="">Example 3</a></li>
+          <li class="list-item"><a href="">Example 4</a></li>
+        </ul>
       </div>
-      <ul class="header-desktop-right">
-        <li class="header-list-item"></li>
-        <li class="header-list-item"></li>
-        <li class="header-list-item"></li>
-        <li class="header-list-item"></li>
-      </ul>
+      <div class="header-desktop-item center">
+        <a href="/" title="Tommy John Home" class="header-logo" aria-label="Tommy John Logo" tabindex="0">
+        TJ Logo
+        </a>
+      </div>
+      <div class="header-desktop-item right">
+        <ul class="header-desktop__list">
+          <li class="header-list-item">Search</li>
+          <li class="header-list-item">Sign-in</li>
+          <li class="header-list-item">Cart</li>
+        </ul>
+      </div>
       <div class="header-mobile-right"></div>
     </div>
   </header>
@@ -28,6 +37,55 @@ export default {
 <style lang="scss">
 
 .header-main {
+    @media screen and (min-width: 1280px) {
+    
+  }
+
+  .header-main__inner {
+    @media screen and (min-width: 1280px) {
+      height: 66px;
+      padding: 0 48px;
+    }
+  }
+
+  .header-desktop-item {
+    display: none;
+
+    @media screen and (min-width: 1280px) {
+      display: inline-block;
+      width: 33.3333%;
+    }
+
+    ul {
+      margin: 0;
+      padding: 0;
+
+      li {
+        list-style-type: none;
+        display: inline-block;
+      }
+    }
+
+    &.left {
+      ul {
+        text-align: left;
+      }
+
+      li {
+        &:not(:last-child){
+          margin-right: 40px;
+        }
+      }
+    }
+
+    &.right{
+      li {
+         &:not(:last-child){
+          margin-right: 24px;
+        }
+      }
+    }
+  }
 
   @media screen and (min-width: 480px) {
 
@@ -42,9 +100,6 @@ export default {
     
   }
    @media screen and (min-width: 1024px) {
-    
-  }
-   @media screen and (min-width: 1280px) {
     
   }
    @media screen and (min-width: 1440px) {
