66fa5a31ac3c2bb1637011338da7ecfde1ced3ed
Serge M <serge@tommyjohnwear.com>
2020-05-29 13:24:30 -0400

    does this work

--- src/lib/sanity.js
+++ src/lib/sanity.js
@@ -2,7 +2,7 @@
 const sanityClient = require('@sanity/client');
 
 export const client = sanityClient({
-  projectId: process.env.PROJECT_ID || 'tw5gcwis',
+  projectId: process.env.PROJECT_ID,
   dataset: 'poc',
   token: process.env.ACCESS_TOKEN, 
   useCdn: false 
